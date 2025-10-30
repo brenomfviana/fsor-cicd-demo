8fc42fe81d9080d158fd53d6df9e6b8fc1321dae
Breno Viana <bmfviana@gmail.com>
2025-10-30 17:30:46 -0300

    atualiza teste

--- tests/test_calc.py
+++ tests/test_calc.py
@@ -8,6 +8,7 @@ from calc import add, subtract, multiply, divide
 def test_add():
     assert add(2, 3) == 5
     assert add(-1, 1) == 0
+    assert add(0, 1) == 1
 
 def test_subtract():
     assert subtract(5, 3) == 2
